eb3169fd6ed2b16a9da3ef2f28d0b1fed4e79a28
Tummanoon Sripechpong <62575162+tummanoon77@users.noreply.github.com>
2020-05-07 20:16:53 -0400

    post api/notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,25 @@
 const path = require("path");
+const fs = require("fs");
 
 
 module.exports = function(app){
 
 
- //GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+ // GET `/api/notes` -  read the `db.json` file and return all saved notes as JSON.
     app.get("/api/notes", (req, res) => {
         let readNotes = fs.readFileSync("/../db/db.json", "utf-8");
         notes = JSON.parse(readNotes);
         res.json(notes);
     });   
 
-
-
+// POST `/api/notes` -  receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+    app.post("/api/notes", (req, res) => {
+        let note = req.body;
+        let readNotes = fs.readFileSync("./db/db.json", "utf-8");
+        let notes = JSON.parse(readNotes);
+        notes.push(note);
+        let writeNotes = JSON.stringify(notes);
+        fs.writeFileSync("./db/db.json", writeNotes);
+        res.json(notes);
+    });
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,16 @@ const path = require("path");
 
 module.exports = function(app){
 
+   // GET `/notes` - Should return the `notes.html` file. 
     app.get("/notes", function(req, res){
         // Send back notes.html file
         res.sendFile(path.join(__dirname,"/../public/notes.html"));
         }); 
+    
+    
+   //  GET `*` - Should return the `index.html` file
     app.get("*", function(req, res){
             // Send back index.html file
         res.sendFile(path.join(__dirname,"/../public/index.html"));
             });
-
-
     }

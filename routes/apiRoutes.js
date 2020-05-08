5380c416c6f9b74f739e09ffd1eaba1207d0b8bd
Tummanoon Sripechpong <62575162+tummanoon77@users.noreply.github.com>
2020-05-07 20:10:18 -0400

    add get read note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,5 +2,15 @@ const path = require("path");
 
 
 module.exports = function(app){
-    
+
+
+ //GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+    app.get("/api/notes", (req, res) => {
+        let readNotes = fs.readFileSync("/../db/db.json", "utf-8");
+        notes = JSON.parse(readNotes);
+        res.json(notes);
+    });   
+
+
+
 }
